7b2df638d390d00223bbd231880e61242a9fbd6c
TheWoxPL <wojtek.bubula@onet.pl>
2026-01-29 17:33:45 +0100

    feat(backend): add cors

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,13 @@ config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE', 'PATCH');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // Middleware
 app.use(bodyParser.json());
 
