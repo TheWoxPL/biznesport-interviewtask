d742ca7a886914aa376332b88af9f8ae7f594624
TheWoxPL <wojtek.bubula@onet.pl>
2026-01-29 14:22:09 +0100

    feat(backend): endpoints for update and delete messages

--- backend/controllers/messagesController.js
+++ backend/controllers/messagesController.js
@@ -30,3 +30,55 @@ export const createMessage = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateMessage = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { message } = req.body;
+
+    const found = await Message.findByPk(id);
+
+    if (!found) {
+      return res.status(404).json({
+        success: false,
+        message: "Message not found",
+      });
+    }
+
+    found.message = message;
+    await found.save();
+
+    res.status(200).json({
+      success: true,
+      data: found,
+    });
+  } catch (error) {
+    error.statusCode = 500;
+    next(error);
+  }
+};
+
+export const deleteMessage = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const found = await Message.findByPk(id);
+
+    if (!found) {
+      return res.status(404).json({
+        success: false,
+        message: "Message not found",
+      });
+    }
+
+    await found.destroy();
+
+    res.status(200).json({
+      success: true,
+      data: { id: Number(id) },
+    });
+  } catch (error) {
+    error.statusCode = 500;
+    next(error);
+  }
+};
